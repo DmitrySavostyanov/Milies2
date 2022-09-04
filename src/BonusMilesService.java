0832847f88a741e10491b4352c2cdb8ec1b74a45
Dmitry Savostyanov <dokken671@gmail.com>
2022-09-04 22:00:21 +0700

    first commit - mod -> gitignore

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int cost) { // входной параметр: cost типа int (подтягивает цену билета)
+        int myMiles;//объявление переменной для количества миль.
+        int percentInKopecks = 20;//стоимость одной бонусной мили в руб.
+        myMiles = cost / percentInKopecks;//расчет количества миль за покупку билета.
+        return myMiles;// возвращает рассчитанное количество миль
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service =new BonusMilesService();
+        int cost = 10_000;//стоимость билета в руб.(входной параметр: cost (в перев. с англ. - стоимость) типа int)
+        int myMiles = service.calculate(cost);
+        System.out.println(myMiles);
+    }
 }
